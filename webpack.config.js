6b2cc648694a58596dec43fb3e2267518a26f4ad
brettlangdon <brett@blangdon.com>
2016-04-11 17:27:41 -0400

    remove unecessary files

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const PluginWebpackConfig = require('graylog-web-plugin').PluginWebpackConfig;
-const loadBuildConfig = require('graylog-web-plugin').loadBuildConfig;
-const path = require('path');
-
-module.exports = new PluginWebpackConfig('io.underdog.GraylogPluginDatadog', loadBuildConfig(path.resolve(__dirname, './build.config')), {
-  // Here goes your additional webpack configuration.
-});
